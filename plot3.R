af694c3922294a1f0770f5cc0b6b04860b7ea0ea
paras1605 <paras1605@gmail.com>
2019-03-20 23:15:55 +0530

    adding missed code

--- plot3.R
+++ plot3.R
@@ -5,3 +5,12 @@ subsetdata$Date <- as.Date(subsetdata$Date, format="%d/%m/%Y")
 # Date has to be used with time and hence combining date and time as one
 datetime <- paste(subsetdata$Date, subsetdata$Time)
 subsetdata$datetime <- as.POSIXct(datetime)
+
+with(subsetdata, {plot(Sub_metering_1 ~ datetime, type = "l", xlab="", ylab="Energy sub metering")
+    lines(Sub_metering_2 ~ datetime, col = "red")
+    lines(Sub_metering_3 ~ datetime, col = "blue")
+    legend("topright", lty = 1, cex = 0.6, bty = "n", col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))}
+)
+
+dev.copy(png, "plot3.png", width = 480, height = 480)
+dev.off()
